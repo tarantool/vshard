482426b69deb29bfbac3356aaf57c1616ac03a00
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2018-01-12 19:30:39 +0300

    test: fix unstable rebalancer test
    * in the test case about disbalance a threshold 1% is too small to
      make the cluster be in the perfect balance - there is 200 buckets and
      99 vs 101 is considered as ok, becase disbalance here is 0.5%. Make
      threshold as 0.01% - it is enough;
    * in the test about bucket lifecycle (active -> sent -> deleted) a garbage
      collector sometimes is too fast and deletes a bucket right after it is
      sent, and before it is detected in the test. Lets wakeup test fiber in this
      place more ofthen.

--- test/main/rebalancer.test.lua
+++ test/main/rebalancer.test.lua
@@ -106,7 +106,7 @@ vshard.storage.rebalancer_enable()
 wait_state('The cluster is balanced ok')
 _bucket.index.status:count({vshard.consts.BUCKET.ACTIVE})
 -- Return 1%.
-vshard.consts.REBALANCER_DISBALANCE_THRESHOLD = 1
+vshard.consts.REBALANCER_DISBALANCE_THRESHOLD = 0.01
 wait_state('Rebalance routes are sent')
 wait_state('The cluster is balanced ok')
 _bucket.index.status:count({vshard.consts.BUCKET.ACTIVE})
@@ -172,7 +172,7 @@ vshard.storage.rebalancer_wakeup()
 -- Now rebalancer makes a bucket SENT. After it the garbage
 -- collector cleans it and deletes after a timeout.
 --
-while _bucket:get{91}.status ~= vshard.consts.BUCKET.SENT do fiber.sleep(0.1) end
+while _bucket:get{91}.status ~= vshard.consts.BUCKET.SENT do fiber.sleep(0.01) end
 while _bucket:get{91} ~= nil do fiber.sleep(0.1) end
 _bucket.index.status:count({vshard.consts.BUCKET.ACTIVE})
 _bucket.index.status:min({vshard.consts.BUCKET.ACTIVE})
