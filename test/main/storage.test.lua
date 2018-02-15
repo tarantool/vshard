c9638bdace4749958de7f230a38fbae32ac7547b
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2018-02-15 23:04:31 +0300

    test: fix flacky storage test

--- test/main/storage.test.lua
+++ test/main/storage.test.lua
@@ -92,6 +92,7 @@ test_run:cmd("start server storage_2_a")
 test_run:cmd("switch storage_2_a")
 fiber = require('fiber')
 while #vshard.storage.info().alerts ~= 1 do fiber.sleep(0.1) end
+fiber.sleep(1)
 vshard.storage.info()
 test_run:cmd("stop server storage_2_b")
 vshard.storage.info()
