896bce78340d7ec66a954a554615ad47223a501d
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2022-06-13 23:50:29 +0200

    storage: handle _bucket changes in commit triggers
    Previously _bucket changes only triggered wakeup of various
    background fibers. They didn't validate anything nor changed.
    
    While the patch doesn't do much on the field of validation (but it
    is a part of the future patches), it makes _bucket updates do some
    changes on commit. Without need to do that after an explicit
    transaction like
    
        _bucket:replace(...)
        bucket_ref.ro_lock = true
    
    Now the replace itself on commit tries to do the right things.
    
    This is important because otherwise replicas don't get any updates
    to bucket_ref objects. For example, if a bucket was ACTIVE and
    became SENT/GARBAGE, and on the replica it had ro refs > 0, then
    on the replica it didn't install `ro_lock` to reject new ro refs.
    So new read requests could come and would be accepted.
    
    Now also when a bucket is deleted or becomes garbage, its ref
    descriptor is deleted. That fixes a similar problem when an
    ACTIVE bucket is just deleted completely.
    
    There are also more updates which handle cases impossible during
    normal functioning, but still could be done manually or happen as
    a bug. For example, it shouldn't be possible that a RECEIVING
    bucket has a ref descriptor at all. Yet if it is detected, the
    ref is deleted.
    
    The patch intentionally doesn't install rw_lock on SENDING bucket,
    because it is supposed to be installed *before* the bucket becomes
    SENDING. Checks like that are a subject of a future patch which
    will make _bucket on_replace trigger do sanity checks before
    commit happens.
    
    The patch is a part of big work on making _bucket updates more
    robust and make replicas aware of and agree with them.
    
    Part of #173

--- test/instances/storage.lua
+++ test/instances/storage.lua
@@ -11,6 +11,7 @@ _G.ilt = require('luatest')
 _G.imsgpack = require('msgpack')
 _G.ivconst = require('vshard.consts')
 _G.ivutil = require('vshard.util')
+_G.iverror = require('vshard.error')
 _G.ivtest = require('test.luatest_helpers.vtest')
 
 _G.iwait_timeout = _G.ivtest.wait_timeout
@@ -37,6 +38,15 @@ end
 box.cfg(helpers.box_cfg())
 local instance_uuid = box.info.uuid
 box.schema.user.grant('guest', 'super', nil, nil, {if_not_exists = true})
+local local_meta = box.schema.create_space('ilocal_meta', {
+    is_local = true,
+    if_not_exists = true,
+    format = {
+        {'key', 'string'},
+        {'value', 'any', is_nullable = true}
+    }
+})
+local_meta:create_index('pk', {if_not_exists = true})
 
 local function box_error()
     box.error(box.error.PROC_LUA, 'box_error')
@@ -77,6 +87,46 @@ local function bucket_gc_wait()
     end)
 end
 
+local function bucket_gc_pause()
+    local errinj = vshard.storage.internal.errinj
+    errinj.ERRINJ_BUCKET_GC_PAUSE = true
+    t.helpers.retrying({timeout = wait_timeout}, function()
+        if errinj.ERRINJ_BUCKET_GC_PAUSE == 1 then
+            return
+        end
+        vshard.storage.garbage_collector_wakeup()
+        error('Bucket GC is still not paused')
+    end)
+end
+
+local function bucket_gc_continue()
+    vshard.storage.internal.errinj.ERRINJ_BUCKET_GC_PAUSE = false
+    vshard.storage.garbage_collector_wakeup()
+end
+
+local function bucket_recovery_pause()
+    local errinj = vshard.storage.internal.errinj
+    errinj.ERRINJ_RECOVERY_PAUSE = true
+    t.helpers.retrying({timeout = wait_timeout}, function()
+        if errinj.ERRINJ_RECOVERY_PAUSE == 1 then
+            return
+        end
+        vshard.storage.recovery_wakeup()
+        error('Bucket recovery is still not paused')
+    end)
+end
+
+local function bucket_recovery_continue()
+    vshard.storage.internal.errinj.ERRINJ_RECOVERY_PAUSE = false
+    vshard.storage.garbage_collector_wakeup()
+end
+
+local function wal_sync()
+    -- Any WAL write guarantees that all the previous WAL writes are finished.
+    -- Local space is used so as to work on replicas too.
+    local_meta:replace{'wal_sync'}
+end
+
 _G.box_error = box_error
 _G.echo = echo
 _G.get_uuid = get_uuid
@@ -84,5 +134,10 @@ _G.get_first_bucket = get_first_bucket
 _G.session_set = session_set
 _G.session_get = session_get
 _G.bucket_gc_wait = bucket_gc_wait
+_G.bucket_gc_pause = bucket_gc_pause
+_G.bucket_gc_continue = bucket_gc_continue
+_G.bucket_recovery_pause = bucket_recovery_pause
+_G.bucket_recovery_continue = bucket_recovery_continue
+_G.wal_sync = wal_sync
 
 _G.ready = true
---
+++ test/storage-luatest/storage_2_test.lua
@@ -0,0 +1,242 @@
+local t = require('luatest')
+local vtest = require('test.luatest_helpers.vtest')
+local vutil = require('vshard.util')
+local vconst = require('vshard.consts')
+local verror = require('vshard.error')
+
+local group_config = {{}}
+
+if vutil.feature.memtx_mvcc then
+    table.insert(group_config, {memtx_use_mvcc_engine = true})
+end
+
+local test_group = t.group('storage', group_config)
+
+local cfg_template = {
+    sharding = {
+        {
+            replicas = {
+                replica_1_a = {
+                    master = true,
+                },
+                replica_1_b = {},
+            },
+        },
+    },
+    bucket_count = 20
+}
+local cluster_cfg
+
+test_group.before_all(function(g)
+    cfg_template.memtx_use_mvcc_engine = g.params.memtx_use_mvcc_engine
+    cluster_cfg = vtest.config_new(cfg_template)
+
+    vtest.storage_new(g, cluster_cfg)
+    vtest.storage_bootstrap(g, cluster_cfg)
+    vtest.storage_wait_vclock_all(g)
+    vtest.storage_rebalancer_disable(g)
+    g.replica_1_a:exec(function()
+        _G.bucket_recovery_pause()
+    end)
+end)
+
+test_group.after_all(function(g)
+    g.cluster:drop()
+end)
+
+local function bucket_refro(bid)
+    local ok, err = ivshard.storage.bucket_refro(bid)
+    ilt.assert_equals(err, nil)
+    ilt.assert(ok)
+end
+
+local function bucket_refro_fail(bid, errcode)
+    local ok, err = ivshard.storage.bucket_refro(bid)
+    ilt.assert(not ok)
+    ilt.assert_equals(err.code, errcode)
+end
+
+local function bucket_unrefro(bid)
+    local ok, err = ivshard.storage.bucket_unrefro(bid)
+    ilt.assert_equals(err, nil)
+    ilt.assert(ok)
+end
+
+local function bucket_gc_wait()
+    _G.bucket_gc_wait()
+end
+
+local function bucket_update_status(bid, status)
+    box.space._bucket:update({bid}, {{'=', 2, status}})
+end
+
+local function bucket_force_create(bid)
+    ivshard.storage.bucket_force_create(bid)
+end
+
+local function bucket_force_drop(bid)
+    ivshard.storage.bucket_force_drop(bid)
+end
+
+local function bucket_check_no_ref(bid)
+    -- When called after GC polling, the bucket can be not visible in _bucket
+    -- already, but not committed yet, and its ref could still be there.
+    _G.wal_sync()
+    ilt.assert_equals(ivshard.storage.internal.bucket_refs[bid], nil)
+end
+
+local function bucket_check_has_ref(bid)
+    ilt.assert_not_equals(ivshard.storage.internal.bucket_refs[bid], nil)
+end
+
+local function bucket_check_ref_ro_lock(bid)
+    ilt.assert(ivshard.storage.internal.bucket_refs[bid].ro_lock)
+end
+
+local function bucket_check_ref_no_ro_lock(bid)
+    ilt.assert(not ivshard.storage.internal.bucket_refs[bid].ro_lock)
+end
+
+--
+-- Test what happens when a bucket is unable to get new reads nor writes.
+--
+local function test_bucket_space_trigger_garbage(g, status)
+    local rep_a = g.replica_1_a
+    local rep_b = g.replica_1_b
+
+    local bid = vtest.storage_first_bucket(rep_a)
+    -- Ref the bucket to make it create a cashed ref object with non-zero refs
+    -- in it. It should prevent its garbage collection until the refs are gone.
+    rep_b:exec(bucket_refro, {bid})
+    rep_a:exec(bucket_refro, {bid})
+
+    -- Turn the bucket into garbage.
+    t.assert_items_include({vconst.BUCKET.SENT, vconst.BUCKET.GARBAGE},
+                           {status})
+    rep_a:exec(bucket_update_status, {bid, status})
+    rep_a:exec(bucket_refro_fail, {bid, verror.code.BUCKET_IS_LOCKED})
+
+    rep_b:wait_vclock_of(rep_a)
+    rep_b:exec(bucket_refro_fail, {bid, verror.code.BUCKET_IS_LOCKED})
+
+    -- Collect the bucket.
+    rep_b:exec(bucket_unrefro, {bid})
+    rep_a:exec(bucket_unrefro, {bid})
+    rep_a:exec(bucket_gc_wait)
+
+    -- Ensure its ref is gone on all nodes.
+    rep_a:exec(bucket_check_no_ref, {bid})
+    rep_b:wait_vclock_of(rep_a)
+    rep_b:exec(bucket_check_no_ref, {bid})
+
+    -- Ensure it works when a bucket has no refs too.
+    rep_a:exec(bucket_force_create, {bid})
+    rep_a:exec(bucket_update_status, {bid, status})
+    rep_a:exec(bucket_gc_wait)
+
+    rep_a:exec(bucket_check_no_ref, {bid})
+    rep_b:wait_vclock_of(rep_a)
+    rep_b:exec(bucket_check_no_ref, {bid})
+
+    -- Cleanup.
+    rep_a:exec(bucket_force_create, {bid})
+    rep_b:wait_vclock_of(rep_a)
+end
+
+local function test_bucket_space_trigger_active(g, status)
+    local rep_a = g.replica_1_a
+    local rep_b = g.replica_1_b
+
+    -- Make a non-active bucket.
+    local bid = vtest.storage_first_bucket(rep_a)
+    rep_a:exec(bucket_update_status, {bid, vconst.BUCKET.SENDING})
+    rep_a:exec(bucket_check_no_ref)
+
+    rep_b:wait_vclock_of(rep_a)
+    rep_b:exec(bucket_check_no_ref)
+
+    -- Bucket turn into an active one works fine when it had no refs.
+    t.assert_items_include({vconst.BUCKET.ACTIVE, vconst.BUCKET.PINNED},
+                           {status})
+    rep_a:exec(bucket_update_status, {bid, status})
+    rep_a:exec(bucket_check_no_ref)
+
+    rep_b:wait_vclock_of(rep_a)
+    rep_b:exec(bucket_check_no_ref)
+
+    -- Turn from a non-active ro-locked bucket back into active is an abnormal
+    -- situation. Nonetheless, should drop the lock then.
+    rep_a:exec(bucket_refro, {bid})
+    rep_b:exec(bucket_refro, {bid})
+
+    rep_a:exec(bucket_update_status, {bid, vconst.BUCKET.SENT})
+    rep_a:exec(bucket_check_ref_ro_lock, {bid})
+
+    rep_b:wait_vclock_of(rep_a)
+    rep_b:exec(bucket_check_ref_ro_lock, {bid})
+
+    rep_a:exec(bucket_update_status, {bid, status})
+    rep_a:exec(bucket_check_ref_no_ro_lock, {bid})
+
+    rep_b:wait_vclock_of(rep_a)
+    rep_b:exec(bucket_check_ref_no_ro_lock, {bid})
+
+    -- Restore.
+    rep_a:exec(bucket_unrefro, {bid})
+    rep_b:exec(bucket_unrefro, {bid})
+    rep_a:exec(bucket_force_drop, {bid})
+    rep_a:exec(bucket_check_no_ref)
+    rep_a:exec(bucket_force_create, {bid})
+    rep_b:wait_vclock_of(rep_a)
+end
+
+test_group.test_bucket_space_trigger_sent = function(g)
+    test_bucket_space_trigger_garbage(g, vconst.BUCKET.SENT)
+end
+
+test_group.test_bucket_space_trigger_garbage = function(g)
+    test_bucket_space_trigger_garbage(g, vconst.BUCKET.GARBAGE)
+end
+
+test_group.test_bucket_space_trigger_active = function(g)
+    test_bucket_space_trigger_active(g, vconst.BUCKET.ACTIVE)
+end
+
+test_group.test_bucket_space_trigger_pinned = function(g)
+    test_bucket_space_trigger_active(g, vconst.BUCKET.PINNED)
+end
+
+test_group.test_bucket_space_triggers_receiving = function(g)
+    local rep_a = g.replica_1_a
+    local rep_b = g.replica_1_b
+
+    -- Commit of RECEIVING bucket works fine when no refs. That is a part of the
+    -- normal process of bucket receipt.
+    local bid = vtest.storage_first_bucket(rep_a)
+    rep_a:exec(bucket_force_drop, {bid})
+    rep_a:exec(bucket_update_status, {bid, vconst.BUCKET.RECEIVING})
+    rep_a:exec(bucket_check_no_ref, {bid})
+
+    rep_b:wait_vclock_of(rep_a)
+    rep_b:exec(bucket_check_no_ref, {bid})
+
+    rep_a:exec(bucket_force_drop, {bid})
+    rep_a:exec(bucket_force_create, {bid})
+    rep_b:wait_vclock_of(rep_a)
+
+    -- Commit of RECEIVING bucket works fine when has refs. Abnormal situation.
+    -- Nonetheless, should drop the refs then.
+    rep_a:exec(bucket_refro, {bid})
+    rep_b:exec(bucket_refro, {bid})
+    rep_a:exec(bucket_check_has_ref, {bid})
+    rep_a:exec(bucket_update_status, {bid, vconst.BUCKET.RECEIVING})
+    rep_a:exec(bucket_check_no_ref, {bid})
+
+    rep_b:wait_vclock_of(rep_a)
+    rep_b:exec(bucket_check_no_ref, {bid})
+
+    -- Restore.
+    rep_a:exec(bucket_force_drop, {bid})
+    rep_a:exec(bucket_force_create, {bid})
+    rep_b:wait_vclock_of(rep_a)
+end
