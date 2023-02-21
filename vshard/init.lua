128eb3a6d5da685da558ce583e38214d5e971e5f
Ilya Grishnov <ilya.grishnov@tarantool.org>
2023-02-21 22:34:57 +0300

    api: add hardcoded versioning support
    Added the _VERSION variable to the exported table.
    Is part of the task [1].
    
    1. github.com/tarantool/roadmap-internal/issues/204
    
    @TarantoolBot document
    Title: vshard module versioning support
    
    Added the ability to get the current version
    of the vshard module as a text variable.
    
    ```Lua
    tarantool> require('vshard')._VERSION
    ---
    - 0.1.22
    ...
    ```
    
    NO_TEST=<implemented via ci>

--- vshard/init.lua
+++ vshard/init.lua
@@ -1,6 +1,9 @@
+local consts = require('vshard.consts')
+
 return {
+    _VERSION = consts.VERSION,
     router = require('vshard.router'),
     storage = require('vshard.storage'),
-    consts = require('vshard.consts'),
+    consts = consts,
     error = require('vshard.error'),
 }
