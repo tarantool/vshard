4752fa08d834a6f76cdfca74abc52e0b41b08389
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2023-03-28 15:44:01 +0200

    test: use spaces in storage/storage_1_1.lua
    This file will be changed in a next commit. Checkpatch is
    complaining if the new lines have tabs. Need to convert the
    entire file to make it consistent.
    
    NO_DOC=refactoring

--- test/storage/storage_1_1.lua
+++ test/storage/storage_1_1.lua
@@ -11,19 +11,19 @@ util = require('util')
 instance_uuid = nil
 replicaset_uuid = nil
 if name == 'storage_1_1' then
-	box.cfg{}
-	instance_uuid = box.info.uuid
-	replicaset_uuid = box.info.cluster.uuid
+    box.cfg{}
+    instance_uuid = box.info.uuid
+    replicaset_uuid = box.info.cluster.uuid
 elseif name == 'storage_1_2' then
-	box.cfg{instance_uuid = '8a274925-a26d-47fc-9e1b-af88ce939412'}
-	instance_uuid = '8a274925-a26d-47fc-9e1b-af88ce000000'
-	replicaset_uuid = box.info.cluster.uuid
+    box.cfg{instance_uuid = '8a274925-a26d-47fc-9e1b-af88ce939412'}
+    instance_uuid = '8a274925-a26d-47fc-9e1b-af88ce000000'
+    replicaset_uuid = box.info.cluster.uuid
 elseif name == 'storage_1_3' then
-	box.cfg{replicaset_uuid = '8a274925-a26d-47fc-9e1b-af88ce939412'}
-	instance_uuid = box.info.uuid
-	replicaset_uuid = '8a274925-a26d-47fc-9e1b-af88ce000000'
+    box.cfg{replicaset_uuid = '8a274925-a26d-47fc-9e1b-af88ce939412'}
+    instance_uuid = box.info.uuid
+    replicaset_uuid = '8a274925-a26d-47fc-9e1b-af88ce000000'
 else
-	assert(false)
+    assert(false)
 end
 
 cfg = {
