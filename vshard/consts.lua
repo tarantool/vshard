38bc420d132261515e1fd2cc08de7cb4fe35720a
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2023-04-05 22:30:50 +0200

    doc: create 0.1.23 changelog
    A new 0.1.23 release
    
    NO_DOC=changelog
    NO_TEST=changelog

--- vshard/consts.lua
+++ vshard/consts.lua
@@ -1,7 +1,7 @@
 return {
     -- Сontains the module version.
     -- Requires manual update in case of release commit.
-    VERSION = '0.1.22',
+    VERSION = '0.1.23',
 
     -- Bucket FSM
     BUCKET = {
