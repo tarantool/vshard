ccccb85dc1add0af117dbc8eb87134811e1de6a1
Nikita Zheleztsov <n.zheleztsov@proton.me>
2023-12-15 21:40:52 +0300

    storage: export name to info
    When named config is used, there may be no UUIDs specified for
    replicas and replicasets. Consequently, in such case we must
    also show the names in addition to UUIDs in storage.info().
    
    Part of #426
    
    @TarantoolBot document
    Title: vshard: vshard.storage.info shows name
    
    When `identification_mode` is `name_as_key`, persisted master and
    replicaset name is shown in vshard.storage.info:
    
    ```
    tarantool> vshard.storage.info()
    ---
    - replicasets:
        replicaset_1:
          name: replicaset_1
          master:
            uri: admin@localhost:3302
            name: replica_1_a
      ...
    ```

--- test/storage-luatest/storage_1_1_test.lua
+++ test/storage-luatest/storage_1_1_test.lua
@@ -544,6 +544,10 @@ test_group.test_named_config_identification = function(g)
 
     -- Attempt to configure with named config without name set causes error.
     g.replica_1_a:exec(function(cfg)
+        -- Name is not shown in info, when uuid identification is used.
+        local rs = ivshard.storage.info().replicasets[ivutil.replicaset_uuid()]
+        ilt.assert_equals(rs.name, nil)
+        ilt.assert_equals(rs.master.name, nil)
         ilt.assert_error_msg_contains('Instance name mismatch',
                                       ivshard.storage.cfg, cfg, 'replica_1_a')
     end, {new_global_cfg})
@@ -574,6 +578,12 @@ test_group.test_named_config_identification = function(g)
     local rs_name_2 = g.replica_2_a:replicaset_name()
     t.assert_equals(rs_name_2, 'replicaset_2')
     local bid = g.replica_1_a:exec(function(name)
+        -- Name is shown, when name identification is used.
+        local rs = ivshard.storage.info().replicasets[box.info.replicaset.name]
+        ilt.assert_equals(rs.name, box.info.replicaset.name)
+        ilt.assert_equals(rs.uuid, nil)
+        ilt.assert_equals(rs.master.name, box.info.name)
+
         local bid = _G.get_first_bucket()
         local ok, err = ivshard.storage.bucket_send(
             bid, name, {timeout = _G.iwait_timeout})
