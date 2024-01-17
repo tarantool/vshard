ab573e590561cb7cbf147456074154e6d4ddf779
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2024-01-17 22:11:39 +0100

    Bump version 0.1.26
    NO_DOC=tag
    NO_TEST=tag

--- vshard/consts.lua
+++ vshard/consts.lua
@@ -1,7 +1,7 @@
 return {
     -- Сontains the module version.
     -- Requires manual update in case of release commit.
-    VERSION = '0.1.25',
+    VERSION = '0.1.26',
 
     -- Bucket FSM
     BUCKET = {
