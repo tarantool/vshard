cea68f3c6769dc65183361fb0b752a83977065d4
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2024-09-23 23:45:28 +0200

    test: use vardir for repository copying
    Upgrade-tests use git to clone the current repository and
    check the necessary versions out.
    
    The cloned repository was always saved in vshard/test/var, even
    when the actual --var argument for test-run.py was /tmp/var.
    
    Lets better make the copied code also stored by the path given
    in --var.
    
    NO_DOC=internal

--- test/reload_evolution/storage_1_a.lua
+++ test/reload_evolution/storage_1_a.lua
@@ -1,16 +1,10 @@
 #!/usr/bin/env tarantool
 local util = require('util')
 NAME = require('fio').basename(arg[0], '.lua')
-
--- Run one storage on a different vshard version.
--- To do that, place vshard src to
--- BUILDDIR/test/var/vshard_git_tree_copy/.
+local source_path = arg[1]
 original_package_path = package.path
 if NAME == 'storage_2_a' then
-    vshard_copy = util.BUILDDIR .. '/test/var/vshard_git_tree_copy'
-    package.path = string.format(
-        '%s/?.lua;%s/?/init.lua;%s',
-        vshard_copy, vshard_copy, original_package_path
-    )
+    package.path = string.format('%s/?.lua;%s/?/init.lua;%s', source_path,
+                                 source_path, package.path)
 end
 require('storage_template')
