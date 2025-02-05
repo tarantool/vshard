eb810a881a4f6ee3997282ed62dc849ade9615d5
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-02-05 08:48:54 +0300

    replicaset: skip unhealthy replicas in all requests
    From now on all requests automatically skip broken instances
    during statefull and stateless request balancing.
    
    Closes tarantool/vshard#505
    
    NO_DOC=internal

--- vshard/replicaset.lua
+++ vshard/replicaset.lua
@@ -375,6 +375,50 @@ local function replica_check_backoff(replica, now)
 end
 
 --
+-- Check if replica can be considered healthy. It's connection wasn't down
+-- for FAILOVER_FOWN_TIMEOUT, the requests didn't fail sequentially for
+-- FAILOVER_DOWN_SEQUENTIAL_FAIL, it has alive replication from master.
+--
+local function replicaset_check_replica_health(replicaset, replica, now)
+    if not replica_check_backoff(replica, now) then
+        return false
+    end
+    -- down_ts not nil does not mean that the replica is not
+    -- connected. Probably it is connected and now fetches schema,
+    -- or does authorization. Either case, it is healthy, no need
+    -- to down the prio.
+    if replica.down_ts and
+       now - replica.down_ts >= consts.FAILOVER_DOWN_TIMEOUT then
+        return false
+    end
+    -- If we failed several sequential requests to replica, then something
+    -- is wrong with it. Temporary lower its priority.
+    if replica.net_sequential_fail >= consts.FAILOVER_DOWN_SEQUENTIAL_FAIL then
+        return false
+    end
+    -- The replication health check must be applied if and only if there's
+    -- node in replicaset with alive replication or master with alive
+    -- connection.
+    local is_replication_alive = false
+    for _, r in pairs(replicaset.replicas) do
+        local is_master = r == replicaset.master
+        if (is_master and r.health_status == consts.STATUS.GREEN) or
+           (not is_master and r.health_status ~= consts.STATUS.RED) then
+            is_replication_alive = true
+            break
+        end
+    end
+    -- If replica doesn't have proper connection with its master: connection
+    -- is dead, lag or idle time is too big - then replica has outdated data
+    -- and shouldn't be used as prioritized.
+    if is_replication_alive and replica.health_status == consts.STATUS.RED then
+        return false
+    end
+    return true
+end
+
+
+--
 -- Connect to a specified replica and remember a new connection
 -- in the replica object. Note, that the function does not wait
 -- until a connection is established.
@@ -824,7 +868,7 @@ local function replicaset_template_multicallro(prefer_replica, balance)
             state.balance_checked_num = state.balance_checked_num + 1
             r = replicaset_balance_replica(replicaset, state)
             if r:is_connected() and (not prefer_replica or r ~= master) and
-               replica_check_backoff(r, now) then
+               replicaset_check_replica_health(replicaset, r, now) then
                 return r
             end
         end
@@ -1744,36 +1788,7 @@ local function replicaset_failover_need_down_priority(replicaset, curr_ts)
     if not r or not r.next_by_priority then
         return false
     end
-    -- down_ts not nil does not mean that the replica is not
-    -- connected. Probably it is connected and now fetches schema,
-    -- or does authorization. Either case, it is healthy, no need
-    -- to down the prio.
-    local is_down_ts = r.down_ts and not r:is_connected() and
-                       curr_ts - r.down_ts >= consts.FAILOVER_DOWN_TIMEOUT
-    -- If we failed several sequential requests to replica, then something
-    -- is wrong with it. Temporary lower its priority.
-    local is_sequential_fails =
-        r.net_sequential_fail >= consts.FAILOVER_DOWN_SEQUENTIAL_FAIL
-    -- The replication health check must be applied if and only if there's
-    -- node in replicaset with alive replication or master with alive
-    -- connection.
-    local is_replication_alive = false
-    for _, r in pairs(replicaset.replicas) do
-        local is_master = r == replicaset.master
-        if (is_master and r.health_status == consts.STATUS.GREEN) or
-           (not is_master and r.health_status ~= consts.STATUS.RED) then
-            is_replication_alive = true
-            break
-        end
-    end
-    -- If replica doesn't have proper connection with its master: connection
-    -- is dead, lag or idle time is too big - then replica has outdated data
-    -- and shouldn't be used as prioritized.
-    local is_unhealthy = false
-    if is_replication_alive then
-        is_unhealthy = r.health_status == consts.STATUS.RED
-    end
-    return is_down_ts or is_sequential_fails or is_unhealthy
+    return not replicaset_check_replica_health(replicaset, r, curr_ts)
 end
 
 --
