tarantool/vshard

commit eb810a881a4f6ee3997282ed62dc849ade9615d5

replicaset: skip unhealthy replicas in all requests
From now on all requests automatically skip broken instances
during statefull and stateless request balancing.

Closes tarantool/vshard#505

NO_DOC=internal

vshard/replicaset.lua
@@ -375,6 +375,50 @@ local function replica_check_backoff(replica, now)
 end
 
 --
+-- Check if replica can be considered healthy. It's connection wasn't down
+-- for FAILOVER_FOWN_TIMEOUT, the requests didn't fail sequentially for
+-- FAILOVER_DOWN_SEQUENTIAL_FAIL, it has alive replication from master.
+--
+local function replicaset_check_replica_health(replicaset, replica, now)
+    if not replica_check_backoff(replica, now) then
+        return false
+    end
+    -- down_ts not nil does not mean that the replica is not
+    -- connected. Probably it is connected and now fetches schema,
+    -- or does authorization. Either case, it is healthy, no need
+    -- to down the prio.
+    if replica.down_ts and
+       now - replica.down_ts >= consts.FAILOVER_DOWN_TIMEOUT then
+        return false
+    end
+    -- If we failed several sequential requests to replica, then something
+    -- is wrong with it. Temporary lower its priority.
+    if replica.net_sequential_fail >= consts.FAILOVER_DOWN_SEQUENTIAL_FAIL then
+        return false
+    end
+    -- The replication health check must be applied if and only if there's
+    -- node in replicaset with alive replication or master with alive
+    -- connection.
+    local is_replication_alive = false
+    for _, r in pairs(replicaset.replicas) do
+        local is_master = r == replicaset.master
+        if (is_master and r.health_status == consts.STATUS.GREEN) or
+           (not is_master and r.health_status ~= consts.STATUS.RED) then
+            is_replication_alive = true
+            break
+        end
+    end
+    -- If replica doesn't have proper connection with its master: connection
+    -- is dead, lag or idle time is too big - then replica has outdated data
+    -- and shouldn't be used as prioritized.
+    if is_replication_alive and replica.health_status == consts.STATUS.RED then
+        return false
+    end
+    return true
+end
+
+
+--
 -- Connect to a specified replica and remember a new connection
 -- in the replica object. Note, that the function does not wait
 -- until a connection is established.
@@ -824,7 +868,7 @@ local function replicaset_template_multicallro(prefer_replica, balance)
             state.balance_checked_num = state.balance_checked_num + 1
             r = replicaset_balance_replica(replicaset, state)
             if r:is_connected() and (not prefer_replica or r ~= master) and
-               replica_check_backoff(r, now) then
+               replicaset_check_replica_health(replicaset, r, now) then
                 return r
             end
         end
@@ -1744,36 +1788,7 @@ local function replicaset_failover_need_down_priority(replicaset, curr_ts)
     if not r or not r.next_by_priority then
         return false
     end
-    -- down_ts not nil does not mean that the replica is not
-    -- connected. Probably it is connected and now fetches schema,
-    -- or does authorization. Either case, it is healthy, no need
-    -- to down the prio.
-    local is_down_ts = r.down_ts and not r:is_connected() and
-                       curr_ts - r.down_ts >= consts.FAILOVER_DOWN_TIMEOUT
-    -- If we failed several sequential requests to replica, then something
-    -- is wrong with it. Temporary lower its priority.
-    local is_sequential_fails =
-        r.net_sequential_fail >= consts.FAILOVER_DOWN_SEQUENTIAL_FAIL
-    -- The replication health check must be applied if and only if there's
-    -- node in replicaset with alive replication or master with alive
-    -- connection.
-    local is_replication_alive = false
-    for _, r in pairs(replicaset.replicas) do
-        local is_master = r == replicaset.master
-        if (is_master and r.health_status == consts.STATUS.GREEN) or
-           (not is_master and r.health_status ~= consts.STATUS.RED) then
-            is_replication_alive = true
-            break
-        end
-    end
-    -- If replica doesn't have proper connection with its master: connection
-    -- is dead, lag or idle time is too big - then replica has outdated data
-    -- and shouldn't be used as prioritized.
-    local is_unhealthy = false
-    if is_replication_alive then
-        is_unhealthy = r.health_status == consts.STATUS.RED
-    end
-    return is_down_ts or is_sequential_fails or is_unhealthy
+    return not replicaset_check_replica_health(replicaset, r, curr_ts)
 end
 
 --

test/router/router2.result
@@ -666,6 +666,19 @@ test_run:switch('router_1')
 fiber.sleep(vshard.consts.REPLICA_BACKOFF_INTERVAL)
  | ---
  | ...
+storage_2 = vshard.router.static.replicasets[replicasets[2]]
+ | ---
+ | ...
+-- Simulate successful ping.
+storage_2_a = storage_2.replicas[util.name_to_uuid.storage_2_a]
+ | ---
+ | ...
+storage_2_a.net_sequential_fail = 0
+ | ---
+ | ...
+storage_2_a.net_sequential_ok = 1
+ | ---
+ | ...
 -- Now goes to the best replica - it is enabled again.
 res, err = vshard.router.callro(1, 'echo', {100}, long_timeout)
  | ---

test/router/router2.test.lua
@@ -267,6 +267,11 @@ vshard.storage.enable()
 test_run:switch('router_1')
 -- Drop the backoff.
 fiber.sleep(vshard.consts.REPLICA_BACKOFF_INTERVAL)
+storage_2 = vshard.router.static.replicasets[replicasets[2]]
+-- Simulate successful ping.
+storage_2_a = storage_2.replicas[util.name_to_uuid.storage_2_a]
+storage_2_a.net_sequential_fail = 0
+storage_2_a.net_sequential_ok = 1
 -- Now goes to the best replica - it is enabled again.
 res, err = vshard.router.callro(1, 'echo', {100}, long_timeout)
 assert(res == 100)

test/router-luatest/router_3_3_test.lua
@@ -112,6 +112,26 @@ local function router_wait_prioritized(g, replica)
     end)
 end
 
+local function router_test_callbro(g, bid, skip_uuids)
+    g.router:exec(function(bid, skip)
+        local uuid = ivshard.router.callbro(bid, 'get_uuid')
+        local uuids, tmp, count = {}, uuid, 0
+        repeat
+            -- Callbro should be non failing, broken ones are skipped.
+            count = count + 1
+            t.assert_not_equals(tmp, nil)
+            t.assert_equals(uuids[tmp], nil)
+            uuids[tmp] = true
+            tmp = ivshard.router.callbro(bid, 'get_uuid')
+        until tmp == uuid
+        -- Every node is covered.
+        ilt.assert_equals(count + #skip, 3)
+        for _, u in ipairs(skip) do
+            ilt.assert_equals(uuids[u], nil)
+        end
+    end, {bid, skip_uuids})
+end
+
 local function failover_health_check_missing_upstream(g)
     router_wait_prioritized(g, g.replica_1_c)
     -- Reconfigure box.cfg.replication.
@@ -121,13 +141,16 @@ local function failover_health_check_missing_upstream(g)
         g.replica_1_b.net_box_uri,
     }})
     -- Prioritized replica is changed to another one.
+    local bid = vtest.storage_first_bucket(g.replica_1_b)
     router_wait_prioritized(g, g.replica_1_b)
+    router_test_callbro(g, bid, {g.replica_1_c:instance_uuid()})
     local msg = 'Replica %s is unhealthy: Missing upstream'
     msg = string.format(msg, g.replica_1_c:instance_uuid())
     t.assert(g.router:grep_log(msg), msg)
     -- Restore replication. Replica returns.
     g.replica_1_c:update_box_cfg({replication = box_cfg.replication})
     router_wait_prioritized(g, g.replica_1_c)
+    router_test_callbro(g, bid, {})
     msg = 'Replica %s is healthy'
     msg = string.format(msg, g.replica_1_c:instance_uuid())
     t.assert(g.router:grep_log(msg), msg)
@@ -161,7 +184,9 @@ local function failover_health_check_broken_upstream(g)
                        upstream.status == 'loading')
         end)
     end, {g.replica_1_a:instance_id()})
+    local bid = vtest.storage_first_bucket(g.replica_1_b)
     router_wait_prioritized(g, g.replica_1_b)
+    router_test_callbro(g, bid, {g.replica_1_c:instance_uuid()})
     -- Either status or idle, depending on version.
     local msg = 'Replica %s is unhealthy'
     msg = string.format(msg, g.replica_1_c:instance_uuid())
@@ -204,7 +229,9 @@ local function failover_health_check_big_lag(g)
             return _G.old_call(service_name, ...)
         end
     end)
+    local bid = vtest.storage_first_bucket(g.replica_1_b)
     router_wait_prioritized(g, g.replica_1_b)
+    router_test_callbro(g, bid, {g.replica_1_c:instance_uuid()})
     local msg = 'Replica %s is unhealthy: Upstream to master has lag'
     msg = string.format(msg, g.replica_1_c:instance_uuid())
     t.assert(g.router:grep_log(msg), msg)
@@ -213,6 +240,7 @@ local function failover_health_check_big_lag(g)
         ivshard.storage._call = _G.old_call
     end)
     router_wait_prioritized(g, g.replica_1_c)
+    router_test_callbro(g, bid, {})
     msg = 'Replica %s is healthy'
     msg = string.format(msg, g.replica_1_c:instance_uuid())
     t.assert(g.router:grep_log(msg), msg)
@@ -302,6 +330,7 @@ local function failover_health_check_missing_master(g)
 
     -- Not changed. Not enough info.
     router_assert_prioritized(g, g.replica_1_c)
+    router_test_callbro(g, vtest.storage_first_bucket(g.replica_1_b), {})
     g.replica_1_c:exec(function(cfg)
         ivshard.storage.cfg(cfg, box.info.uuid)
     end, {old_cfg})

commit c642650d023093ce750bae52481db3dadcd5d367

replicaset: speed up the read-only calls
Commit eb810a881a4f6ee39972 ("replicaset: skip unhealthy replicas in
all requests") caused major degradation of performance in read-only
calls, because it added the bunch of checks for every call when
searching for the healthy replica.

This commit makes updating health asynchronous by moving the status
check to failover service. Since we need to have access for the health
of all replicas, it's located in the replicaset's failover.

Even though the update of health status is asynchronous, in some cases
we can update it right away: backoff and sequential fails. And not only
we can, it has to be updated as soon as possible, since the speed of a
replica being moved to RED status affects the latency of the user
directly. But we cannot do that for connection being down fot N seconds
or for replication checks, so they remain asynchronous. That behavior is
already tested in router/router2.test and
router-luatest/router_3_3_test.

Closes #568

NO_DOC=bugfix

vshard/replicaset.lua
@@ -117,6 +117,17 @@ local replica_errinj = {
 }
 
 --
+-- Since state can be set from several places, in order to react on the failures
+-- as fast as we can, this tables allows to use unified messages.
+--
+local health_status_msgs = {
+    ['BACKOFF'] = "'%s' is in backoff",
+    ['CONN_DOWN'] = "Connection to '%s' was down for %.5f seconds",
+    ['SEQ_FAIL'] = "Requests to '%s' failed %d times in a row",
+    ['REPLICATION'] = "Replication from master on '%s' is broken"
+}
+
+--
 -- vconnect is asynchronous vshard greeting, saved inside netbox connection.
 -- It stores future object and additional info, needed for its work.
 -- Future is initialized, when the connection is established (inside
@@ -392,12 +403,12 @@ end
 --
 -- Check if replica can be considered healthy. It's connection wasn't down
 -- for FAILOVER_FOWN_TIMEOUT, the requests didn't fail sequentially for
--- failover_sequential_fail_count (cfg option), it has alive replication
--- from master.
+-- failover_sequential_fail_count (cfg option).
 --
-local function replicaset_check_replica_health(replicaset, replica, now)
+local function replica_check_health(replica, now)
+    local name = replica.name or replica.uuid
     if not replica_check_backoff(replica, now) then
-        return false
+        return SRED, string.format(health_status_msgs.BACKOFF, name)
     end
     -- down_ts not nil does not mean that the replica is not
     -- connected. Probably it is connected and now fetches schema,
@@ -405,33 +416,17 @@ local function replicaset_check_replica_health(replicaset, replica, now)
     -- to down the prio.
     if replica.down_ts and
        now - replica.down_ts >= consts.FAILOVER_DOWN_TIMEOUT then
-        return false
+        return SRED, string.format(health_status_msgs.CONN_DOWN,
+                                   name, now - replica.down_ts)
     end
     -- If we failed several sequential requests to replica, then something
     -- is wrong with it. Temporary lower its priority.
     if replica.net_sequential_fail >=
        replica.failover_sequential_fail_count then
-        return false
+        return SRED, string.format(health_status_msgs.SEQ_FAIL,
+                                   name, replica.net_sequential_fail)
     end
-    -- The replication health check must be applied if and only if there's
-    -- node in replicaset with alive replication or master with alive
-    -- connection.
-    local is_replication_alive = false
-    for _, r in pairs(replicaset.replicas) do
-        local is_master = r == replicaset.master
-        if (is_master and r.replication_status == SGREEN) or
-           (not is_master and r.replication_status ~= SRED) then
-            is_replication_alive = true
-            break
-        end
-    end
-    -- If replica doesn't have proper connection with its master: connection
-    -- is dead, lag or idle time is too big - then replica has outdated data
-    -- and shouldn't be used as prioritized.
-    if is_replication_alive and replica.replication_status == SRED then
-        return false
-    end
-    return true
+    return SGREEN
 end
 
 
@@ -615,6 +610,13 @@ end
 local function replica_on_failed_request(replica)
     replica.net_sequential_ok = 0
     replica.net_sequential_fail = replica.net_sequential_fail + 1
+    if replica.net_sequential_fail >=
+       replica.failover_sequential_fail_count then
+        local msg = string.format(health_status_msgs.SEQ_FAIL,
+                                  replica.name or replica.uuid,
+                                  replica.net_sequential_fail)
+        replica:update_status('health_status', SRED, msg)
+    end
     if replica.net_sequential_fail >= 2 then
         local new_timeout = replica.net_timeout * 2
         if new_timeout <= consts.CALL_TIMEOUT_MAX then
@@ -869,7 +871,7 @@ local function replicaset_template_multicallro(prefer_replica, balance)
     --
     -- Pick a next replica according to round-robin load balancing policy.
     --
-    local function pick_next_replica(replicaset, now, state)
+    local function pick_next_replica(replicaset, state)
         local r
         local master = replicaset.master
         local i = #replicaset.priority_list
@@ -885,7 +887,7 @@ local function replicaset_template_multicallro(prefer_replica, balance)
             state.balance_checked_num = state.balance_checked_num + 1
             r = replicaset_balance_replica(replicaset, state)
             if r:is_connected() and (not prefer_replica or r ~= master) and
-               replicaset_check_replica_health(replicaset, r, now) then
+               r.health_status ~= SRED then
                 return r
             end
         end
@@ -925,7 +927,7 @@ local function replicaset_template_multicallro(prefer_replica, balance)
         local now = fiber_clock()
         local end_time = now + timeout
         while not net_status and timeout > 0 do
-            replica = pick_next_replica(replicaset, now, state)
+            replica = pick_next_replica(replicaset, state)
             if not replica then
                 replica, timeout = replicaset_wait_master(replicaset, timeout)
                 if not replica then
@@ -966,6 +968,9 @@ local function replicaset_template_multicallro(prefer_replica, balance)
                                  consts.REPLICA_BACKOFF_INTERVAL, retval)
                         replica.backoff_ts = now
                         replica.backoff_err = retval
+                        local msg = string.format(health_status_msgs.BACKOFF,
+                                                  replica.name or replica.uuid)
+                        replica:update_status('health_status', SRED, msg)
                     end
                 else
                     -- There is no sense to retry LuaJit errors, such as
@@ -1631,6 +1636,7 @@ local function buildall(sharding_cfg)
                 failover_sequential_fail_count = sharding_cfg[count_name],
                 failover_interval = sharding_cfg.failover_interval,
                 failover_replica_lag_limit = sharding_cfg[lag_name],
+                health_status = SYELLOW,
             }, replica_mt)
             new_replicaset.replicas[replica_id] = new_replica
             if replica.master then
@@ -1810,15 +1816,41 @@ end
 --------------------------------------------------------------------------------
 
 --
+-- Update the health_status of all replicas in the replicaset.
+--
+local function replicaset_failover_update_health(replicaset, now)
+    -- The replication health check must be applied iff there's node in
+    -- replicaset with alive replication or master with alive connection.
+    local is_replication_alive = false
+    for _, r in pairs(replicaset.replicas) do
+        local is_master = r == replicaset.master
+        if (is_master and r.replication_status == SGREEN) or
+           (not is_master and r.replication_status ~= SRED) then
+            is_replication_alive = true
+            break
+        end
+    end
+    for _, r in pairs(replicaset.replicas) do
+        local status, reason = replica_check_health(r, now)
+        if is_replication_alive and status < r.replication_status then
+            status = r.replication_status
+            reason = string.format(health_status_msgs.REPLICATION,
+                                   r.name or r.uuid)
+        end
+        replica_update_status(r, 'health_status', status, reason)
+    end
+end
+
+--
 -- Replicaset must fall its replica connection to lower priority,
 -- if the current one is down too long.
 --
-local function replicaset_failover_need_down_priority(replicaset, curr_ts)
+local function replicaset_failover_need_down_priority(replicaset)
     local r = replicaset.replica
     if not r or not r.next_by_priority then
         return false
     end
-    return not replicaset_check_replica_health(replicaset, r, curr_ts)
+    return r.health_status == SRED
 end
 
 --
@@ -1841,11 +1873,12 @@ local function replicaset_failover_step(replicaset)
         return false
     end
     local curr_ts = fiber_clock()
+    replicaset_failover_update_health(replicaset, curr_ts)
     local old_replica = replicaset.replica
     if replicaset_failover_need_up_priority(replicaset, curr_ts) then
         replicaset:up_replica_priority()
     end
-    if replicaset_failover_need_down_priority(replicaset, curr_ts) then
+    if replicaset_failover_need_down_priority(replicaset) then
         replicaset:down_replica_priority()
     end
     if old_replica ~= replicaset.replica then

test/replicaset-luatest/replicaset_3_test.lua
@@ -33,7 +33,7 @@ local cfg_template = {
 local global_cfg
 
 test_group.before_all(function(g)
-    global_cfg = vtest.config_new(cfg_template)
+    global_cfg = vcfg.check(vtest.config_new(cfg_template))
 
     vtest.cluster_new(g, global_cfg)
     vtest.cluster_bootstrap(g, global_cfg)
@@ -221,7 +221,7 @@ local function get_auto_master_global_cfg()
     local rs_cfg = new_cfg_template.sharding[1]
     rs_cfg.master = 'auto'
     rs_cfg.replicas.replica_1_a.master = nil
-    return vtest.config_new(new_cfg_template)
+    return vcfg.check(vtest.config_new(new_cfg_template))
 end
 
 test_group.test_locate_master_when_no_conn_object = function(g)

test/router/router2.result
@@ -473,10 +473,10 @@ test_run:switch('router_1')
  | ---
  | - true
  | ...
-conn = vshard.router.route(1).master.conn
+master = vshard.router.route(1).master
  | ---
  | ...
-ok, err = pcall(conn.call, conn, 'vshard.storage.call',                         \
+ok, err = pcall(master.conn.call, conn, 'vshard.storage.call',                  \
                 {1, 'read', 'echo', {1}})
  | ---
  | ...
@@ -485,6 +485,17 @@ assert(not ok and err.code == box.error.NO_SUCH_PROC)
  | - true
  | ...
 
+function wait_replica_restore(r)                                                \
+    while master.health_status ~= vshard.consts.STATUS.GREEN do                 \
+        failover_wakeup()                                                       \
+        fiber.sleep(0.01)                                                       \
+    end                                                                         \
+end
+ | ---
+ | ...
+wait_replica_restore(master)
+ | ---
+ | ...
 -- Works when called via vshard - it goes to another replica.
 res = vshard.router.callro(1, 'echo', {100}, long_timeout)
  | ---
@@ -563,7 +574,7 @@ test_run:switch('router_1')
  | ---
  | - true
  | ...
-fiber.sleep(vshard.consts.REPLICA_BACKOFF_INTERVAL)
+wait_replica_restore(master)
  | ---
  | ...
 rs = vshard.router.route(1)
@@ -594,7 +605,7 @@ test_run:switch('router_1')
  | - true
  | ...
 -- Drop old backoffs.
-fiber.sleep(vshard.consts.REPLICA_BACKOFF_INTERVAL)
+wait_replica_restore(master)
  | ---
  | ...
 -- Success, but internally the request was retried.
@@ -663,7 +674,7 @@ test_run:switch('router_1')
  | - true
  | ...
 -- Drop the backoff.
-fiber.sleep(vshard.consts.REPLICA_BACKOFF_INTERVAL)
+wait_replica_restore(master)
  | ---
  | ...
 storage_2 = vshard.router.static.replicasets[replicasets[2]]

test/router/router2.test.lua
@@ -188,11 +188,18 @@ vshard.storage.call = nil
 
 -- Indeed fails when called directly via netbox.
 test_run:switch('router_1')
-conn = vshard.router.route(1).master.conn
-ok, err = pcall(conn.call, conn, 'vshard.storage.call',                         \
+master = vshard.router.route(1).master
+ok, err = pcall(master.conn.call, conn, 'vshard.storage.call',                  \
                 {1, 'read', 'echo', {1}})
 assert(not ok and err.code == box.error.NO_SUCH_PROC)
 
+function wait_replica_restore(r)                                                \
+    while master.health_status ~= vshard.consts.STATUS.GREEN do                 \
+        failover_wakeup()                                                       \
+        fiber.sleep(0.01)                                                       \
+    end                                                                         \
+end
+wait_replica_restore(master)
 -- Works when called via vshard - it goes to another replica.
 res = vshard.router.callro(1, 'echo', {100}, long_timeout)
 assert(res == 100)
@@ -224,7 +231,7 @@ vshard.storage.call = old_storage_call
 -- Fails without backoff for other errors.
 --
 test_run:switch('router_1')
-fiber.sleep(vshard.consts.REPLICA_BACKOFF_INTERVAL)
+wait_replica_restore(master)
 rs = vshard.router.route(1)
 ok, err = rs:callro('vshard.storage.call', {1, 'badmode', 'echo', {100}},       \
                     long_timeout)
@@ -238,7 +245,7 @@ vshard.storage.disable()
 
 test_run:switch('router_1')
 -- Drop old backoffs.
-fiber.sleep(vshard.consts.REPLICA_BACKOFF_INTERVAL)
+wait_replica_restore(master)
 -- Success, but internally the request was retried.
 --
 -- n/a: there was a bug when an error code in the router depended
@@ -266,7 +273,7 @@ vshard.storage.enable()
 
 test_run:switch('router_1')
 -- Drop the backoff.
-fiber.sleep(vshard.consts.REPLICA_BACKOFF_INTERVAL)
+wait_replica_restore(master)
 storage_2 = vshard.router.static.replicasets[replicasets[2]]
 -- Simulate successful ping.
 storage_2_a = storage_2.replicas[util.name_to_uuid.storage_2_a]