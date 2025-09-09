0e5b03f356dd1c33480f7a6278b4590a04505654
Roman Gromov <r.gromov.official@gmail.com>
2025-09-09 16:28:55 +0300

    rebalancer: add logging of routes
    This patch adds rebalancer routes' logging. The log file now
    includes information about the source storage, the number of
    buckets, and the destination storage where the buckets will
    be moved.
    
    Since the rebalancer service has changed logging of routes that
    were sent, we change the `rebalancer/rebalancer.test.lua` and
    `rebalancer/stress_add_remove_several_rs.test.lua` tests.
    
    Part of tarantool/vshard#212
    
    NO_DOC=bugfix

--- vshard/storage/init.lua
+++ vshard/storage/init.lua
@@ -2918,8 +2918,9 @@ local function rebalancer_service_f(service, limiter)
                 goto continue
             end
         end
-        log.info('Rebalance routes are sent. Schedule next wakeup after '..
-                 '%f seconds', consts.REBALANCER_WORK_INTERVAL)
+        log.info('The following rebalancer routes were sent: %s. ' ..
+                 'Schedule next wakeup after %f seconds', json_encode(routes),
+                 consts.REBALANCER_WORK_INTERVAL)
         service:set_activity('idling')
         lfiber.testcancel()
         lfiber.sleep(consts.REBALANCER_WORK_INTERVAL)
