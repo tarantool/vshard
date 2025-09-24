6fdf404ca203a5dfe27eb9c44674e22edfb7d1c8
Roman Gromov <r.gromov.official@gmail.com>
2025-09-24 17:03:02 +0300

    ssl: check no sensitive ssl data in logs
    The `safe_uri` function is used in vshard to get a purged uri. Before
    tarantool#11810 patch sensitive ssl data may leaked into logs as
    `uri.format` function didn't handle ssl params of uri.
    
    This patch explicitly sets `write_sensitive` parameter of `uri.format`
    into `false` in `safe_uri` function and add some tests which check that
    sensitive ssl data doesn't leak into logs of vshard storage and router.
    
    NO_DOC=bugfix
    
    Closes tarantool/vshard#593

--- vshard/replicaset.lua
+++ vshard/replicaset.lua
@@ -1416,7 +1416,7 @@ local replica_mt = {
         safe_uri = function(replica)
             local uri = luri.parse(replica.uri)
             uri.password = nil
-            return util.uri_format(uri)
+            return util.uri_format(uri, false)
         end,
         connect = replica_connect,
         detach_conn = replica_detach_conn,
