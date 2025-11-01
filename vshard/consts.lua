b86421ab64798bb9b1d02b3a06617e0e235660de
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-11-01 21:19:31 +0300

    Bump version to 0.1.38
    NO_DOC=tag
    NO_TEST=tag

--- vshard/consts.lua
+++ vshard/consts.lua
@@ -1,7 +1,7 @@
 return {
     -- Сontains the module version.
     -- Requires manual update in case of release commit.
-    VERSION = '0.1.37',
+    VERSION = '0.1.38',
 
     -- Bucket FSM
     BUCKET = {
